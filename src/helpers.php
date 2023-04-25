28e2073edae70bf5d4e4d56b03970cec76672b3c
Majid Mohammadian <majeedmohammadian@gmail.com>
2023-04-26 02:41:12 +0430

    add service and helper function

--- src/helpers.php
+++ src/helpers.php
@@ -1,2 +1,19 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
+use JTranslation;
+
+if(!function_exists('translation')) {
+    /**
+     * Service store translation
+     *
+     * @param Model $model
+     * @param array $data
+     *
+     * @return void
+     */
+    function translation(Model $model, array $data = []): string
+    {
+        return JTranslation::store($model, $data);
+    }
+}
