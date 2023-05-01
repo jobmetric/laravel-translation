aa24db46e647485da2324232530cdd9d6d8bf023
Majid Mohammadian <majeedmohammadian@gmail.com>
2023-05-02 02:39:23 +0430

    add cache time config

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Time
+    |--------------------------------------------------------------------------
+    |
+    | Cache time for get data translation
+    |
+    | - set zero for remove cache
+    | - set null for forever
+    |
+    | - unit: minutes
+    */
+
+    "cache_time" => env("TRANSLATION_CACHE_TIME", 0),
+
+];
