25fc88e5c707528f96d025f75063358196e94031
majid mohammadian <majeedmohammadian@gmail.com>
2024-01-05 21:44:33 +0330

    change service provider based on package core

--- src/TranslationServiceProvider.php
+++ src/TranslationServiceProvider.php
@@ -2,66 +2,27 @@
 
 namespace JobMetric\Translation;
 
-use Illuminate\Support\ServiceProvider;
 use JobMetric\Metadata\MetadataServiceProvider;
+use JobMetric\PackageCore\Exceptions\DependencyPublishableClassNotFoundException;
+use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
 
-class TranslationServiceProvider extends ServiceProvider
+class TranslationServiceProvider extends PackageCoreServiceProvider
 {
-    public function register()
-    {
-        $this->app->bind('JTranslation', function ($app) {
-            return new JTranslation($app);
-        });
-
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'j-translation');
-    }
-
     /**
-     * boot provider
-     *
-     * @return void
+     * @throws MigrationFolderNotFoundException
+     * @throws RegisterClassTypeNotFoundException
+     * @throws DependencyPublishableClassNotFoundException
      */
-    public function boot(): void
+    public function configuration(PackageCore $package): void
     {
-        $this->registerMigrations();
-        $this->registerPublishables();
-
-        // set translations
-        $this->loadTranslationsFrom(realpath(__DIR__.'/../lang'), 'j-translation');
-    }
-
-    /**
-     * Register the Passport migration files.
-     *
-     * @return void
-     */
-    protected function registerMigrations(): void
-    {
-        if($this->app->runningInConsole()) {
-            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        }
-    }
-
-    /**
-     * register publishables
-     *
-     * @return void
-     */
-    protected function registerPublishables(): void
-    {
-        if($this->app->runningInConsole()) {
-            // run dependency publishable
-            $this->publishes(self::pathsToPublish(MetadataServiceProvider::class), 'metadata');
-
-            // publish config
-            $this->publishes([
-                realpath(__DIR__.'/../config/config.php') => config_path('j-translation.php')
-            ], 'translation-config');
-
-            // publish migration
-            $this->publishes([
-                realpath(__DIR__.'/../database/migrations') => database_path('migrations')
-            ], 'translation-migrations');
-        }
+        $package->name('laravel-translation')
+            ->hasConfig()
+            ->hasTranslation()
+            ->hasMigration()
+            ->registerDependencyPublishable(MetadataServiceProvider::class)
+            ->registerClass('Translation', Translation::class);
     }
 }
