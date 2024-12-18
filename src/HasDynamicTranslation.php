de430375752c3e1595a549023d57fa02b1e18f43
majidmohammadian <majeedmohammadian@gmail.com>
2024-12-18 21:57:41 +0330

    rename TranslationWithType to HasDynamicTranslation based on new algorithm and change version package core to 1.18.5

---
+++ src/HasDynamicTranslation.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace JobMetric\Translation;
+
+use JobMetric\Translation\ServiceType\Translation;
+
+/**
+ * HasDynamicTranslation
+ *
+ * @property static array $dynamicTranslation
+ */
+trait HasDynamicTranslation
+{
+    protected static array $dynamicTranslation = [];
+
+    /**
+     * Boot Has Dynamic Translation
+     *
+     * @return void
+     */
+    public static function bootHasDynamicTranslation(): void
+    {
+        $serviceType = getServiceTypeClass(static::class);
+
+        $types = $serviceType->getTypes();
+
+        foreach ($types as $type) {
+            $innerType = $serviceType->type($type);
+
+            foreach ($innerType->getTranslation() as $translation) {
+                /**
+                 * @var Translation $translation
+                 */
+                self::$dynamicTranslation[$type][] = $translation->customField->params['uniqName'];
+            }
+        }
+    }
+
+    /**
+     * translation allow fields.
+     *
+     * @return array
+     */
+    public function translationAllowFields(): array
+    {
+        return self::$dynamicTranslation[$this->{$this->dynamicTranslationFieldTypeName()}] ?? [];
+    }
+
+    /**
+     * translation filed type name.
+     *
+     * @return string
+     */
+    public function dynamicTranslationFieldTypeName(): string
+    {
+        return 'type';
+    }
+}
