f1ad68ecbce292492b2f27d111b2345056e88e5f
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-30 01:26:32 +0330

    added translation array request file

---
+++ src/Http/Requests/TranslationArrayRequest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace JobMetric\Translation\Http\Requests;
+
+use JobMetric\Translation\Rules\TranslationFieldExistRule;
+
+trait TranslationArrayRequest
+{
+    public function renderTranslationFiled(
+        array    &$rules,
+        array    $data,
+        string   $model_name,
+        string   $field_name = 'name',
+        int|null $object_id = null,
+        int|null $parent_id = -1,
+        array    $parent_where = []
+    ): void
+    {
+        if (array_key_exists('translation', $data)) {
+            $translation = $data['translation'];
+
+            $locale = '';
+            foreach ($translation as $key => $value) {
+                $locale = $key;
+                break;
+            }
+        } else {
+            $locale = app()->getLocale();
+        }
+
+        $rules["translation"] = 'array';
+        $rules["translation.$locale"] = 'array';
+        $rules["translation.$locale.$field_name"] = [
+            'required',
+            'string',
+            new TranslationFieldExistRule($model_name, $field_name, $locale, $object_id, $parent_id, $parent_where)
+        ];
+
+        $translations = (new $model_name)->translationAllowFields();
+
+        foreach ($translations as $item) {
+            if ($item !== $field_name) {
+                $rules["translation.$locale.$item"] = 'string|nullable|sometimes';
+            }
+        }
+    }
+
+    public function renderTranslationAttribute(
+        array  &$params,
+        array  $data,
+        string $model_name,
+        string $trans_scope = null,
+    ): void
+    {
+        if (array_key_exists('translation', $data)) {
+            $translation = $data['translation'];
+
+            $locale = '';
+            foreach ($translation as $key => $value) {
+                $locale = $key;
+                break;
+            }
+        } else {
+            $locale = app()->getLocale();
+        }
+
+        $translations = (new $model_name)->translationAllowFields();
+
+        foreach ($translations as $item) {
+            $params["translation.$locale.$item"] = trans(str_replace('{field}', $item, $trans_scope));
+        }
+    }
+}
