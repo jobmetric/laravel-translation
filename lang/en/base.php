1344cbcc452e626af35b163de25825f2b7aa0a02
majidmohammadian <majeedmohammadian@gmail.com>
2025-08-27 22:28:37 +0330

    move files to panelify/translation package

--- lang/en/base.php
+++ lang/en/base.php
@@ -17,43 +17,4 @@ return [
         "default_field" => "Name",
     ],
 
-    "components" => [
-        "translation_card" => [
-            "title" => "General Information",
-            "multi_title" => "General Information in :language Language",
-            "tabs" => [
-                "basic_info" => "Basic Information",
-                "seo" => "SEO Settings",
-            ],
-            "fields" => [
-                "name" => [
-                    "label" => "Name",
-                    "info" => "Name of the item",
-                    "placeholder" => "Enter Item Name",
-                ],
-                "meta_title" => [
-                    "label" => "Meta Title",
-                    "info" => "Set a meta title tag. It is recommended to be simple and precise keywords.",
-                    "placeholder" => "Enter Meta Title",
-                ],
-                "meta_description" => [
-                    "label" => "Meta Description",
-                    "info" => "Set a meta description tag to increase SEO rank.",
-                    "placeholder" => "Enter Meta Description",
-                ],
-                "meta_keywords" => [
-                    "label" => "Meta Keywords",
-                    "info" => "Set a list of keywords that this item is related to. Separate keywords by adding <code>,</code> between each keyword.",
-                    "placeholder" => "Enter Meta Keywords",
-                ],
-            ],
-        ],
-    ],
-
-    "modals" => [
-        "translation_list" => [
-            "title" => "Edit Translation in {language} Language",
-        ],
-    ],
-
 ];
--- src/TranslationServiceProvider.php
+++ src/TranslationServiceProvider.php
@@ -2,43 +2,24 @@
 
 namespace JobMetric\Translation;
 
-use Illuminate\Support\Facades\Blade;
 use JobMetric\Metadata\MetadataServiceProvider;
-use JobMetric\PackageCore\Exceptions\AssetFolderNotFoundException;
 use JobMetric\PackageCore\Exceptions\DependencyPublishableClassNotFoundException;
 use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
-use JobMetric\PackageCore\Exceptions\ViewFolderNotFoundException;
 use JobMetric\PackageCore\PackageCore;
 use JobMetric\PackageCore\PackageCoreServiceProvider;
-use JobMetric\Translation\View\Components\TranslationCard;
 
 class TranslationServiceProvider extends PackageCoreServiceProvider
 {
     /**
      * @throws MigrationFolderNotFoundException
      * @throws DependencyPublishableClassNotFoundException
-     * @throws ViewFolderNotFoundException
-     * @throws AssetFolderNotFoundException
      */
     public function configuration(PackageCore $package): void
     {
         $package->name('laravel-translation')
             ->hasConfig()
-            ->hasView()
-            ->hasAsset()
             ->hasTranslation()
             ->hasMigration()
             ->registerDependencyPublishable(MetadataServiceProvider::class);
-    }
-
-    /**
-     * After Boot Package
-     *
-     * @return void
-     */
-    public function afterBootPackage(): void
-    {
-        // add alias for components
-        Blade::component(TranslationCard::class, 'translation-card');
     }
 }
